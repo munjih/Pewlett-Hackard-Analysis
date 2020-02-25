5a5bba796dc81abc94acdd25c4a7f0218258d33b
munjih <59781578+munjih@users.noreply.github.com>
2020-02-24 18:58:51 -0800

    modified schema file

--- schema.sql
+++ schema.sql
@@ -13,8 +13,7 @@ CREATE TABLE employees (
 	 last_name VARCHAR NOT NULL,
 	 gender VARCHAR NOT NULL,
      hire_date DATE NOT NULL,
-	 PRIMARY KEY (emp_no),
-	 UNIQUE (emp_no)
+	 PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_manager (
@@ -37,13 +36,12 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-  dept_no VARCHAR NOT NULL,
   emp_no INT NOT NULL,
+  dept_no VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE titles (
@@ -55,4 +53,6 @@ CREATE TABLE titles (
   PRIMARY KEY (emp_no)
 );
 
-SELECT * FROM departments;
+SELECT * FROM dept_emp;
+
+DROP TABLE dept_emp CASCADE;
